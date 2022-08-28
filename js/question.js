878160dc12b2d0fb39d13fddcf74d31d1d9ff68f
erika397 <guerreroerika408@gmail.com>
2022-08-27 22:36:35 -0500

    Se agrega el proyecto

---
+++ js/question.js
@@ -0,0 +1,22 @@
+(function(){
+    const titleQuestions =[...document.querySelectorAll('.question_title')];
+    titleQuestions.forEach(question=>{
+      question.addEventListener('click',()=>{
+        let height =0;
+        let answer = question.nextElementSibling;
+        let addPadding = question.parentElement.parentElement;
+
+        addPadding.classList.toggle('question_padding--add');
+
+        question.children[0].classList.toggle ('question_arrow--rotate');
+
+        if(answer.clientHeight ===0){
+           height=answer.scrollHeight;
+        }
+        
+        answer.style.height = `${height}px`;
+          
+      });
+      
+    });
+})();
